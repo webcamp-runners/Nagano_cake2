3635a438a6a821918ed8f459bdbc561f756497d9
NaoyaKawa <reventon27urobolos@gmail.com>
2021-01-19 12:10:15 +0000

    add all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      root_path
+    when Admin
+      
+    end
+  end
 end
