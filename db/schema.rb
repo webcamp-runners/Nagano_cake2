eee9eabbe1cb4919714d1d5bc0ad0b1ebd086257
EC2 Default User <ec2-user@ip-172-31-8-2.ec2.internal>
2021-01-19 04:37:53 +0000

    makemodel

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_19_032906) do
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "total_payment"
+    t.integer "shipping_cost"
+    t.string "name"
+    t.string "address"
+    t.string "post_code"
+    t.integer "status"
+    t.integer "payment_method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
