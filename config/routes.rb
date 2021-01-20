0948a424ff855dac1871a5e7162b2165b0039b35
adt718 <ksgses531@gmail.com>
2021-01-20 07:44:19 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :customers
   devise_for :admins
-  
+
+
   namespace :admin do
     get "home" => "homes#top"
   end
-  
+
   scope module: :customer do
     get "home" => "homes#top"
   end
-  
+
 end
