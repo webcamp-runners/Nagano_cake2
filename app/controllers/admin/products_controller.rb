3dc186fc83d51a537fa1767524fc2353f3c3aab7
NaoyaKawa <reventon27urobolos@gmail.com>
2021-01-21 06:52:12 +0000

    add controller view products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,7 +8,7 @@ def show
 end
 
 def new
-
+  @products = Product.new
 end
 
 def edit
@@ -20,6 +20,14 @@ def update
 end
 
 def create
-
+  products = Product.new(product_params)
+  products.save
+  redirect_to products_path
 end
+
+private
+  def product_params
+    params.require(:products).permit(:name, :image,:introduction)
+  end
+  
 end
--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,11 +1,15 @@
 class Customer::ProductsController < ApplicationController
 
   def index
-  
+    @products = Product.all
   end
 
   def show
 
   end
   
+  private
+	def product_params
+		parmas.require(:product).permit(:name, :image, :introduction)
+	end
 end
