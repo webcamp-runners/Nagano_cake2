ec1319a583dd3ae27ebedfb681bb00e3101d9181
seeya1127 <seeya11271995@gmail.com>
2021-01-22 04:57:18 +0000

    create devise and address view

--- config/routes.rb
+++ config/routes.rb
@@ -22,23 +22,27 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :create, :update, :destroy] do
       collection do
         delete 'destroy_all'
-      end
     end
-    resources :customers, only: [:show, :edit, :update] do
+      end
+      resources :customers, only: [:show, :edit, :update] do
       collection do
         get 'unsubscribe'
         patch 'withdraw'
+        get "my_page" => "customers#show"
       end
     end
+    resources :products, only: [:index, :show]
+    resources :addresses, except: [:new, :show]
+  end
+  
     resources :orders, only: [:new, :index, :create] do
       collection do
         get 'confirm'
         get 'complete'
-        get "my_page" => "customers#"
       end
     end
-    resources :addresses, except: [:new, :show]
-    resources :products, only: [:index, :show]
+
+
 
   #管理者側ルート
   namespace :admin do
