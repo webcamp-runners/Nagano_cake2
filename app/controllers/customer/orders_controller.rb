2804d598286576f1def62ba85201dea72f1f7678
seeya1127 <seeya11271995@gmail.com>
2021-01-23 04:55:58 +0000

    edit address's page and order's page

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,5 +1,7 @@
 class Customer::OrdersController < ApplicationController
+  
   def new
+    @order = Order.new
   end
 
   def confirm
--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -61,6 +61,7 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number])
   end
+
 end
