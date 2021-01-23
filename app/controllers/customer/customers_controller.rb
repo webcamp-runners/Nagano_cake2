9e7a9d9748e024456e4ba2584865e5e9e6e2dfdd
EC2 Default User <ec2-user@ip-172-31-8-2.ec2.internal>
2021-01-23 04:56:24 +0000

    onthewey

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -4,23 +4,28 @@ class Customer::CustomersController < ApplicationController
   end
 
   def edit
-
+    @customer = current_customer
   end
 
   def update
-
+    @customer = current_customer
+    @customer.update(update_params)
+    redirect_to my_page_customers_path(@customer.id), notice: 'お客様情報を更新しました。'
   end
 
   def unsubscribe
-
+    @customer = current_customer
   end
 
   def withdraw
-    
+    @customer = current_customer
+    @customer.update(is_deleted: false)
+    reset_session
+    redirect_to new_customer_session_path
   end
 
   private
   def update_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number)
   end
 end
