cca3cfc13d00ff9622c7984c4e9aa07643780f43
seeya1127 <seeya11271995@gmail.com>
2021-01-25 04:22:40 +0000

    fix addresses layout

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :address, presence: true
+  validates :post_code, presence: true, format: {with: /\A\d{7}\z/}
+  validates :name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
 end
